e3a408b3bfbac814538913bc0842a82f2c9d28c1
Dustin J. Mitchell <dustin@mozilla.com>
2015-09-23 14:21:22 -0400

    Bug 1181153: update to use the new treestatus API

--- treestat@roach.at/defaults/preferences/defaults.js
+++ treestat@roach.at/defaults/preferences/defaults.js
@@ -1,4 +1,4 @@
 pref("extensions.treestat.trees", "mozilla-central|mozilla-inbound|try");
-pref("extensions.treestat.rooturl", "https://treestatus.mozilla.org/");
+pref("extensions.treestat.rooturl", "https://api.pub.build.mozilla.org/treestatus/");
 pref("extensions.treestat.frequency", 300);
 pref("extensions.treestat.gemsize", 16);
